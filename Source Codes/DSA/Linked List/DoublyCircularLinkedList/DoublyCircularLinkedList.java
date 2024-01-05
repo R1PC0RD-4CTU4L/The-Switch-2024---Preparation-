74c0d4b1365850b017794ce2e06d3ddb9e3e12bb
dev_sagnikchakraborty@gmail.com <dev.sagnikchakraborty@gmail.com>
2024-01-05 17:06:37 +0530

    Commit 1 Date-05/01/2024

--- Source Codes/DSA/Linked List/DoublyCircularLinkedList/DoublyCircularLinkedList.java
+++ Source Codes/DSA/Linked List/DoublyCircularLinkedList/DoublyCircularLinkedList.java
@@ -84,10 +84,172 @@ public class DoublyCircularLinkedList {
 
     /* insertNode(String value,int position) Insert Node At Any Given Position */
 
+    public void insertNode(String value, int position) {
+
+        /* Case 1- If linked list is not created before */
+        if (head == null) {
+            createLinkedList(value);
+            return;
+        }
+
+        /* Case 2- If node is added at the beginning of the linked list */
+
+        else if (position == 1) {
+            insertNodeAtFirst(value);
+        }
+
+        /* Case 3- If node is added at the end of the linked list */
+        else if (position > sizeOfLinkedList) {
+            insertNodeAtLast(value);
+        }
+
+        /* Case 4- If position is negative or zero. Invalid Case */
+        else {
+            insertNodeAtLast(value);
+        }
+
+    }
+
     /* printList() - Prints the linked list */
 
     public void printList() {
 
+        Node tempNode = head;
+
+        int count = 1;
+        while (count != sizeOfLinkedList + 1) {
+
+            if (count == sizeOfLinkedList) {
+
+                System.out.print(tempNode.value + " --> ");
+                tempNode = tempNode.next;
+                count++;
+                continue;
+            }
+            System.out.print(tempNode.value + " <--> ");
+            tempNode = tempNode.next;
+            count++;
+        }
+
+        System.out.print("HEAD \n");
+
+        /*
+         * for (int i = 1; i <= sizeOfLinkedList; i++) {
+         * 
+         * if (i == sizeOfLinkedList) {
+         * System.out.print(tempNode.value + " --> ");
+         * tempNode = tempNode.next;
+         * continue;
+         * }
+         * 
+         * System.out.print(tempNode.value + " <--> ");
+         * tempNode = tempNode.next;
+         * }
+         * 
+         * System.out.print("HEAD \n");
+         */
     }
 
+    public void deleteNodeFromFirst() {
+
+        if (head == null) {
+            System.out.println("Nothing To Delete!");
+        }
+
+        else if (sizeOfLinkedList == 1) {
+            head = null;
+            tail = null;
+
+            System.out.println("Last Node Deleted. Nothing Else To Delete!");
+            sizeOfLinkedList--;
+        } else {
+
+            head = head.next;
+            tail.next = head;
+            sizeOfLinkedList--;
+            System.out.println("Node Deleted From First");
+        }
+
+    }
+
+    public void deleteNodeFromLast() {
+
+        if (head == null) {
+            System.out.println("Nothing To Delete!");
+        }
+
+        else if (sizeOfLinkedList == 1) {
+            head = null;
+            tail = null;
+
+            System.out.println("Last Node Deleted. Nothing Else To Delete!");
+            sizeOfLinkedList--;
+        } else {
+
+            tail = tail.prev;
+            tail.next = head;
+            sizeOfLinkedList--;
+            System.out.println("Node Deleted From Last!");
+
+        }
+
+    }
+
+    public void deleteNode(int position) {
+        if (head == null) {
+            System.out.println("Nothing To Delete!");
+        }
+
+        else if (sizeOfLinkedList == 1) {
+            head = null;
+            tail = null;
+
+            System.out.println("Last Node Deleted. Nothing Else To Delete!");
+            sizeOfLinkedList--;
+        } else if (position == 1) {
+            deleteNodeFromFirst();
+        } else if (position >= sizeOfLinkedList) {
+            deleteNodeFromLast();
+        } else if (position <= 0) {
+            System.out.println("invalid position");
+        } else {
+
+            Node tempNode = head;
+            for (int i = 1; i < position - 1; i++) {
+
+                tempNode = tempNode.next;
+            }
+
+            Node toBeDeleted = tempNode.next;
+            Node nextNode = tempNode.next.next;
+            toBeDeleted.prev = null;
+            toBeDeleted.next = null;
+            tempNode.next = nextNode;
+            sizeOfLinkedList--;
+            System.out.println("Node Deleted From Position " + position);
+
+        }
+
+    }
+
+    public int searchNode(String value) {
+        Node tempNode = head;
+        for (int i = 1; i < sizeOfLinkedList; i++) {
+            if (tempNode.value == value) {
+                System.out.println("Node found at position " + i);
+                return i;
+            }
+        }
+
+        System.out.println("Value Not Found in Linked List!");
+        return -1;
+    }
+
+    public void deleteLinkedList() {
+        head = null;
+        tail = null;
+
+        sizeOfLinkedList = 0;
+        System.out.println("Linked List Successfully Deleted!");
+    }
 }
--- Source Codes/DSA/Linked List/DoublyCircularLinkedList/main.java
+++ Source Codes/DSA/Linked List/DoublyCircularLinkedList/main.java
@@ -1,3 +1,32 @@
 public class main {
 
+    public static void main(String[] args) {
+
+        DoublyCircularLinkedList dcll = new DoublyCircularLinkedList();
+        dcll.insertNodeAtFirst("Cypher");
+        dcll.insertNodeAtLast("Jett");
+        dcll.insertNodeAtLast("Sage");
+        dcll.insertNodeAtLast("Brimstone");
+        dcll.insertNodeAtLast("Raze");
+        dcll.insertNodeAtLast("Phoenix");
+        dcll.insertNodeAtLast("Omen");
+
+        dcll.printList();
+
+        dcll.deleteNodeFromFirst();
+        dcll.printList();
+
+        System.out.println(dcll.tail.value);
+        System.out.println(dcll.tail.next.value);
+        System.out.println(dcll.head.value);
+
+        dcll.deleteNodeFromLast();
+        dcll.printList();
+
+        System.out.println(dcll.tail.value);
+        System.out.println(dcll.tail.next.value);
+        System.out.println(dcll.head.value);
+
+    }
+
 }
